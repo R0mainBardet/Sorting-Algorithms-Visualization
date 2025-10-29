d6536279a158089141acd49b90e4f4868a8d8c66
Romain Bardet <romain.bardet@live.fr>
2025-10-29 13:51:05 +0100

    window created

--- test.c
+++ test.c
@@ -2,22 +2,46 @@
 #include <stdio.h>
 
 int main() {
-    // Create a window
-    SDL_Window* window = SDL_CreateWindow("Baptiste la salope",
+
+    // Initialiser SDL et verifie si il y a des erreurs d'initialisation
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    int n = 100;
+    int array[n];
+    int win_width = 800;
+    int win_height = 600;
+
+    for(int i = 0; i < n; i++) {
+        array[i] = n - i;
+    }
+    printf("%d", array[0]);
+
+    // Creer une fenetre SDL 
+    SDL_Window* window = SDL_CreateWindow("Sorting algorithm visualization",
                                           SDL_WINDOWPOS_CENTERED,
                                           SDL_WINDOWPOS_CENTERED,
-                                          800, 600,
+                                          win_width, win_height,
                                           SDL_WINDOW_SHOWN);
+    // Verifie si la fenetre est creee correctement sinon retourne une erreur
     if (!window) {
         printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
         SDL_Quit();
         return 1;
     }
+
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); // white bars
+    SDL_RenderFillRect(renderer, &bar);
     
-    // Wait 3 seconds
+    // Delai de 3000 milliseconds (3 seconds)
     SDL_Delay(3000);
 
-    // Destroy window and quit SDL
+    // detruire la fenetre et quitter SDL
     SDL_DestroyWindow(window);
     SDL_Quit();
 
